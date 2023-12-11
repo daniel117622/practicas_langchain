51f3be5cdfb5b5885e18579c39bc7e84988cbb49
Cecoban - Daniel A. Cruz Salazar <DCruz@cecoban.org.mx>
2023-12-11 17:12:44 -0600

    pinecone

--- working_with_files.py
+++ working_with_files.py
@@ -1,4 +1,4 @@
-import dotenv
+import os
 from langchain.chat_models import ChatOpenAI
 from langchain.prompts import *
 from langchain.memory import *
@@ -6,11 +6,18 @@ from langchain.chains import LLMChain , SequentialChain
 from langchain.document_loaders import TextLoader
 from langchain.text_splitter import CharacterTextSplitter
 from langchain.embeddings import OpenAIEmbeddings
-from langchain.vectorstores import Chroma
+from langchain.vectorstores import Redis
+import pinecone
+import uuid
 
-API_KEY = dotenv.get_key(".env","OPENAI_API_KEY")
+file = os.open(".env" , os.O_RDONLY)
+
+pinecone.init(api_key=API_PINECONE , environment="gcp-starter")
+
+API_KEY = str(os.read(file,128)).split("=")[1][0:-1]
+print(f"KEY: {API_KEY}----------")
 chat = ChatOpenAI(openai_api_key=API_KEY)
-embeddings = OpenAIEmbeddings()
+embeddings = OpenAIEmbeddings(openai_api_key=API_KEY)
 
 # When using a summary we need to add a llm.
 memory = ConversationSummaryMemory(
@@ -28,19 +35,26 @@ text_splitter = CharacterTextSplitter(
 )
 
 loader = TextLoader("facts.txt")
-docs = loader.load_and_split(text_splitter=text_splitter)
+pre_docs = loader.load_and_split(text_splitter=text_splitter)
+docs = []
+for d in pre_docs:
+    docs.append(d.page_content)
 
-# Create a small database
-db = Chroma.from_documents(
-    docs,                   # Calls openai to generate embeddings
-    embedding=embeddings,   # Reference to openai
-    persist_directory="emb" # Directory of db
-)
+# This runs only if db is not created
+index_name = "langchain-vectorstore"
+if index_name not in pinecone.list_indexes():
+    # we create a new index
+    pinecone.create_index(
+        name=index_name,
+        metric='cosine',
+        dimension=1536  # 1536 dim of text-embedding-ada-002
+    )
 
-results = db.similarity_search_with_score("What is an interesting language?")
-for result in results:
-    print("\n")
-    print(result[1])
-    print(results[0].page_content)
+    index = pinecone.Index(index_name)
 
+    embeds = embeddings.embed_documents(docs)
+    ids = [str(hash(_)) for _ in range(len(docs))]
+    index.upsert(vectors=zip(ids,embeds))
 
+index = pinecone.Index(index_name)
+print(index.describe_index_stats())
